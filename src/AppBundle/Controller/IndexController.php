640bd3b0c6db051a502606e16d6b93558582d3ef
Lucien Akchoté <l.akchote@gmail.com>
2017-07-05 10:22:59 +0200

    - Création landing page responsive * Création feuilles de style "main.css" et "landing_page.css" * Création vue "landing_page.html.twig" * Création fichier JS "landing_page.js" (pour chargement des modales connexion + inscription / appels AJAX) * Ajout icônes FontAwesome * Modification vue de base "base.html.twig" * Modification de IndexController -> si l'user n'est pas connecté on affiche la landing page sinon on le redirige vers le flux d'annonces

---
+++ src/AppBundle/Controller/IndexController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function indexAction()
+    {
+        return (!$this->getUser()) ? $this->redirectToRoute('landing_page') : $this->redirectToRoute('home');
+    }
+
+    /**
+     * @Route("/lp", name="landing_page")
+     */
+    public function landingPageAction()
+    {
+        return $this->render('default/landing_page.html.twig');
+    }
+
+    /**
+     * @Route("/home", name="home")
+     */
+    public function homeAction()
+    {
+
+    }
+}
---
+++ web/js/landing_page.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.modal-load__register').click(function (e) {
+        e.preventDefault();
+    });
+    $('.modal-load__connect').click(function (e) {
+        e.preventDefault();
+    });
+});
