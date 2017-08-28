653d2bd93275e1c7430a3388b1c39d841440be3d
Lucien Akchoté <l.akchote@gmail.com>
2017-08-28 14:14:57 +0200

    - Refactoring controller AnnonceController * Actions relatives aux annonces désormais dans le nouveau controller AnnonceActionsController

--- src/AppBundle/Controller/AnnonceController.php
+++ src/AppBundle/Controller/AnnonceController.php
@@ -5,10 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Annonce;
 use AppBundle\Entity\Categorie;
 use AppBundle\Entity\User;
-use AppBundle\Form\Type\AnnonceFormType;
-use AppBundle\Service\PostAnnonce;
 use AppBundle\Service\SetIntroMessagesAsRead;
-use AppBundle\Service\UserAnnoncesActions;
 use Doctrine\ORM\EntityManager;
 use Knp\Component\Pager\Paginator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
@@ -17,44 +14,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Router;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 /**
  * @Security("is_granted('IS_AUTHENTICATED_REMEMBERED')")
  */
 class AnnonceController extends Controller
 {
-    /**
-     * @Route("/annonce/post", name="annonce_post")
-     */
-    public function annoncePostAction(Request $request, PostAnnonce $postAnnonce, Router $router)
-    {
-        if(!$request->isXmlHttpRequest()) return new Response('Type de requête invalide', 400);
-        $form = $this->createForm(AnnonceFormType::class);
-        $form->handleRequest($request);
-        if($form->isValid()) {
-            $postAnnonce->createAdvert($form->getData());
-            $this->addFlash('success', 'Votre annonce a été publiée.');
-            return (new Response())->setContent($router->generate('home'), 200);
-        } else {
-            return (new Response())
-                ->setStatusCode(400)
-                ->setContent($this->renderView('modal/postAdvert.html.twig', [
-                    'form' => $form->createView()
-                ]));
-        }
-    }
-
-    /**
-     * @Route("/annonce/fulltext/{id}", name="annonce_fulltext")
-     */
-    public function annonceFullTextAction(Request $request, EntityManager $em, Annonce $id)
-    {
-        if(!$request->isXmlHttpRequest() || !$annonce = $em->getRepository('AppBundle:Annonce')->find($id)) return new Response('Type de requête invalide', 400);
-        return (new Response())->setContent($annonce->getContenu());
-    }
-
     /**
      * @Route("/annonce/view/{id}", name="annonce_view")
      * @Security("is_granted('IS_AUTHENTICATED_ANONYMOUSLY')")
@@ -64,28 +29,6 @@ class AnnonceController extends Controller
         return $this->render('default/annonce_view.html.twig', [
             'annonce' => $id
         ]);
-    }
-
-    /**
-     * @Route("/annonce/report/{id}", name="annonce_report")
-     */
-    public function annonceReportAction(Request $request, Annonce $id, UserAnnoncesActions $userActions, TokenStorage $tokenStorage)
-    {
-        $currentUser = $tokenStorage->getToken()->getUser();
-        if(!$request->isXmlHttpRequest() || in_array($id->getId(), $currentUser->getSignalementsAnnonces())) return new Response('Type de requête invalide', 400);
-        $userActions->reportAdvert($currentUser, $id);
-        return new Response('', 200);
-    }
-
-    /**
-     * @Route("/annonce/unreport/{id}", name="annonce_unreport")
-     */
-    public function annonceUnreportAction(Request $request, Annonce $id, UserAnnoncesActions $userActions, TokenStorage $tokenStorage)
-    {
-        $currentUser = $tokenStorage->getToken()->getUser();
-        if(!$request->isXmlHttpRequest()) return new Response('Type de requête invalide', 400);
-        $userActions->unReportAdvert($currentUser, $id);
-        return new Response('', 200);
     }
 
     /**
@@ -144,15 +87,5 @@ class AnnonceController extends Controller
             'user' => $user,
             'categorie' => $categorie
         ]);
-    }
-
-    /**
-     * @Route("/annonce/delete/{id}", name="annonce_delete")
-     */
-    public function annonceDeleteAction(Annonce $id, UserAnnoncesActions $userActions)
-    {
-        $userActions->deleteAdvert($id);
-        $this->addFlash('success', 'L\'annonce a été supprimée.');
-        return $this->redirectToRoute('home');
     }
 }
